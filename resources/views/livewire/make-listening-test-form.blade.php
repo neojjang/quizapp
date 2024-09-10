30681239d80caa716bbe87ab65de52d75df07087
neojjang <neojjang@gmail.com>
2024-09-11 00:42:39 +0900

    "업로드취소" 버튼의 동작이 이상하여 수정 작업중

--- resources/views/livewire/make-listening-test-form.blade.php
+++ resources/views/livewire/make-listening-test-form.blade.php
@@ -9,25 +9,24 @@
                         <form wire:submit.prevent >
                             <label class="flex items-center">
                                 <span class="text-gray-700">듣기평가 파일 : &nbsp;</span>
-{{--                                <input  type="file" wire:model="mp3File" name="mp3File" id="mp3File" accept="audio/mp3" wire:loading.attr="disabled"/>--}}
-                                @if($mp3File)
-                                    <span class="text-gray-600 font-bold"> {{$mp3File->getClientOriginalName()}}</span>&nbsp;
-                                    <button wire:click="cancelUploadFile" id="cancelUploadFile"
-                                            class="inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition" > 업로드 취소 </button>
-                                @else
-                                    <input  type="file" wire:model="mp3File" name="mp3File" id="mp3File" accept="audio/mp3" wire:loading.attr="disabled"/>
-                                @endif
+{{--                                @if($mp3File)--}}
+{{--                                    <span class="text-gray-600 font-bold"> {{$mp3File->getClientOriginalName()}}</span>&nbsp;--}}
+{{--                                @else--}}
+                                    <input type="file" wire:model="mp3File" name="mp3File" id="mp3File" value="{{$mp3File}}"  accept="audio/mp3" wire:loading.attr="disabled"/>
+                                    <div wire:loading wire:target="mp3File">업로드중...
+                                        <svg class="inline-flex animate-spin h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                        </svg>
+                                    </div>
+{{--                                @endif--}}
+                                <button wire:click="cancelUploadFile" id="cancelUploadFile"
+                                        class="inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" > 업로드 취소 </button>
                             </label>
-                            <div wire:loading wire:target="mp3File">업로드중...
-                                <svg class="inline-flex animate-spin h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                </svg>
-                            </div>
                             @error('mp3File')<label><span class="text-red-500">{{$message}}</span></label>@enderror
 
-                        </form>
-                        <form wire:submit.prevent>
+{{--                        </form>--}}
+{{--                        <form wire:submit.prevent>--}}
 
                             <label class="flex items-center">
                                 <span class="text-gray-700">전체 문제 수</span>
