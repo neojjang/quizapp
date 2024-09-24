aebd08ee301f239120e196143974472eb2f0e3c7
yjchang <neojjang@gmail.com>
2024-09-24 17:11:28 +0900

    듣기 시험지 수정 작업중
    - AWS 오류 수정

--- resources/views/admins/create_answer_sheet.blade.php
+++ resources/views/admins/create_answer_sheet.blade.php
@@ -3,7 +3,16 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             [{{$section->class_room->name}}] {{$section->name}} {{ __('문제지 만들기') }}
         </h2>
-</x-slot>
+    </x-slot>
+    @if(session()->has('success'))
+        <h2 class="text-medium font-bold rounded-xl bg-green-400 px-2 text-white">
+            {{session('success')}}
+        </h2>
+    @elseif(session()->has('error'))
+        <h2 class="text-medium font-bold rounded-xl bg-red-400 px-2 text-white">
+            {{session('error')}}
+        </h2>
+    @endif
 <div class="max-w-7xl m-1 mx-auto sm:px-6 lg:px-8">
 <!-- Pass the variables and load Livewire component -->
 <livewire:make-answer-sheet-form :section="$section" :question_types="$question_types"/>
